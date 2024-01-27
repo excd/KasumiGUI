5c90c162db66dea6c27db1bfda5b65ce70a1d337
Excd <greggseamon@gmail.com>
2024-01-26 19:50:10 -0500

    Improve logger

--- src/utility/Logger.cs
+++ src/utility/Logger.cs
@@ -1,6 +1,6 @@
 ﻿using Discord;
 
-namespace KasumiGUI.utility {
+namespace KasumiGUI.Utility {
     internal class Logger {
         private readonly Window window;
 
@@ -8,9 +8,14 @@ namespace KasumiGUI.utility {
             this.window = window;
         }
 
-        public Task Log(LogMessage message) {
+        public Task LogAsync(LogMessage message) {
             if (window.IsHandleCreated)
-                return Task.Run(() => window.Invoke(new Action(() => window.Out(message.ToString()))));
+                return Task.Run(() => window.Invoke(new Action(() => {
+                    window.Out(string.Format("[{0}][{1,8}][{2,7}] {3}", DateTime.Now, message.Source, message.Severity, message.Message));
+
+                    if (message.Exception != null)
+                        window.Out(message.Exception.ToString());
+                })));
             else
                 return Task.CompletedTask;
         }
