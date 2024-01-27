57784e6cf02531725201c2d5cf69c58f4ad380d5
Excd <greggseamon@gmail.com>
2024-01-26 19:49:46 -0500

    Create CommandHandler.cs

---
+++ src/Discord/CommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System.Reflection;
+
+namespace KasumiGUI.Discord {
+    internal class CommandHandler {
+        private readonly DiscordSocketClient client;
+        private readonly CommandService commands;
+
+        public CommandHandler(ref DiscordSocketClient client, ref CommandService commands) {
+            this.client = client;
+            this.commands = commands;
+        }
+
+        public async Task InitializeAsync() {
+            client.MessageReceived += HandleCommandAsync;
+            await commands.AddModulesAsync(assembly: Assembly.GetEntryAssembly(), services: null);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage messageParam) {
+            if (messageParam is not SocketUserMessage message || message.Author.IsBot)
+                return;
+
+            int argPos = 0;
+            if (!message.HasCharPrefix('!', ref argPos))
+                return;
+
+            SocketCommandContext context = new(client, message);
+            await commands.ExecuteAsync(context: context, argPos: argPos, services: null);
+        }
+    }
+}
