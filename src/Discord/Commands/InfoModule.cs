8ff2e1ed7485a5fb11a648f2d3fa8140ae2e8ec4
Excd <greggseamon@gmail.com>
2024-01-26 19:50:00 -0500

    Create simple command module

---
+++ src/Discord/Commands/InfoModule.cs
@@ -0,0 +1,27 @@
+﻿using Discord;
+using Discord.Commands;
+
+namespace KasumiGUI.Discord.Commands {
+    public class InfoModule : ModuleBase<SocketCommandContext> {
+        [Command("say")]
+        [Summary("Echoes a message.")]
+        public async Task EchoAsync([Remainder][Summary("The text to echo.")] string message) {
+            await LogCommandAsync(Context.Message.ToString());
+            await ReplyAsync(message);
+        }
+
+        private async Task LogCommandAsync(string command) {
+            if (Program.Logger != null)
+                await Program.Logger.LogAsync(new LogMessage(LogSeverity.Info, "Command",
+                    $"User {Context.User.Username}#{Context.User.Discriminator} executed: {command}"));
+        }
+
+        private async Task ReplyAsync(string message) {
+            if (Program.Logger != null)
+                await Program.Logger.LogAsync(new LogMessage(LogSeverity.Info, "Response",
+                    $"Bot replied: {message}"));
+
+            await base.ReplyAsync(message);
+        }
+    }
+}
